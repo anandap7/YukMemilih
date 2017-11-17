bfe3652affacd6cb3851948850610e506dbc80df
Ananda Priatama <apriatama7@gmail.com>
2017-11-18 02:20:10 +0700

    Initial commit

---
+++ app/src/main/java/com/project/kuyk/yukmemilih/Menu/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.project.kuyk.yukmemilih.Menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.kuyk.yukmemilih.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
